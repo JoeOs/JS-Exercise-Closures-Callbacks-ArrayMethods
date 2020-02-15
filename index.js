22cd82fbd1f5274e8fc5d496d3bf7515cf59af5c
JoeOs <JoeOs@github.com>
2020-02-15 14:12:56 -0500

    passing 17

--- index.js
+++ index.js
@@ -246,8 +246,10 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  let num = runners.reduce((accumulator, runner) => accumulator + runner.donation, 0);
+  return num;
+
 }
 
 /////////////// CLOSURES ///////////////
