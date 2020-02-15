a709c95533755587b03e6d1d5fe2dda46849b640
JoeOs <JoeOs@github.com>
2020-02-15 14:02:23 -0500

    passing 12

--- index.js
+++ index.js
@@ -208,8 +208,9 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let namesAllCap = runners.map(runner => runner.first_name.toUpperCase());
+  return namesAllCap;
 }
 
 /**
